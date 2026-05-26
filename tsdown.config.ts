d978df11fdee449ab634f3b11010c47eb6cde7ac
Shay Cojocaru <shayc@outlook.com>
2026-05-27 00:35:47 +0300

    fix: deprecated

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: ["./src/index.ts"],
   format: ["esm"],
-  external: ["react", "react/jsx-runtime"],
+  deps: { neverBundle: ["react", "react/jsx-runtime"] },
   dts: true,
   sourcemap: true,
   target: "es2024",
